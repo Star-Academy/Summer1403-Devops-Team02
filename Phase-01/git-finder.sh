d9f8b0ebf52845472b8628c42d7c19033735ac07
Alireza Jafari <alirezaja1384@gmail.com>
2024-07-21 22:44:20 +0330

    feat(Phase-01): Default search argument (TODO)

--- Phase-01/git-finder.sh
+++ Phase-01/git-finder.sh
@@ -3,5 +3,5 @@ set -e
 
 toplevel_path="$(git rev-parse --show-toplevel)"
 branches=$(git branch --format='%(refname:short)') 
-git grep "$1" ${branches} -- "${toplevel_path}"
+git grep "${1:-TODO}" ${branches} -- "${toplevel_path}"
 
