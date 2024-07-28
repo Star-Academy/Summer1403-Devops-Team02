3abfca821721666f7d91673157fd834350021b18
Alireza Jafari <alirezaja1384@gmail.com>
2024-07-28 16:09:55 +0330

    feat(Phase-05): `traceroute` Rest API implemented.
    Co-authored-by: Mohammad Sadegh Montazeri <msm1984@users.noreply.github.com>

---
+++ Phase-05/storage.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var (
+	ctx               = context.Background()
+	rdb *redis.Client = nil
+)
+
+func initRedis() {
+	if rdb == nil {
+		rdb = redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "",
+			DB:       0,
+		})
+	}
+}
+
+func saveToRedis(key string, value []byte) error {
+	if rdb == nil {
+		initRedis()
+	}
+
+	err := rdb.Set(ctx, key, value, 0).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
